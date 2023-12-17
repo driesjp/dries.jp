b0d048570662d75095ee39df21f3f95cda9576e8
driesjp <110052701+driesjp@users.noreply.github.com>
2023-12-17 01:21:15 +0100

    Update commands.js
    Updating music links to Bandcamp for 'cli'

--- commands.js
+++ commands.js
@@ -19,11 +19,11 @@ register_cmd("exit", function() {
 
 // Music
 register_cmd("music", function() {
-    block_log("<br><a target='_new' href='https://distrokid.com/hyperfollow/driesjp/the-last-collection-of-weird-synthy-stuff'><img src='icons/music/tlcowss.jpg' /> the last collection of weird synthy stuff</a></br>");
-    block_log("<br><a target='_new' href='https://distrokid.com/hyperfollow/dries/waverunner'><img src='icons/music/waverunner.jpg' /> waverunner</a></br>");
-    block_log("<br><a target='_new' href='https://distrokid.com/hyperfollow/dries/ninja-world-original-video-game-soundtrack'><img src='icons/music/nw.jpg' /> ninja world</a></br>");
-    block_log("<br><a target='_new' href='https://distrokid.com/hyperfollow/dries/ok-computer-do-handel'><img src='icons/music/okdh.jpg' /> ok computer, do handel</a></br>");
-    block_log("<br><a target='_new' href='https://distrokid.com/hyperfollow/dries/-'><img src='icons/music/eibeyuf.jpg' /> everyone is beautiful. even you, ugly fish.</a></br>");
+    block_log("<br><a target='_new' href='https://driesjp.bandcamp.com/album/the-last-collection-of-weird-synthy-stuff'><img src='icons/music/tlcowss.jpg' /> the last collection of weird synthy stuff</a></br>");
+    block_log("<br><a target='_new' href='https://driesjp.bandcamp.com/album/waverunner'><img src='icons/music/waverunner.jpg' /> waverunner</a></br>");
+    block_log("<br><a target='_new' href='https://driesjp.bandcamp.com/album/ninja-world-original-video-game-soundtrack'><img src='icons/music/nw.jpg' /> ninja world</a></br>");
+    block_log("<br><a target='_new' href='https://driesjp.bandcamp.com/album/ok-computer-do-handel'><img src='icons/music/okdh.jpg' /> ok computer, do handel</a></br>");
+    block_log("<br><a target='_new' href='https://driesjp.bandcamp.com/album/-'><img src='icons/music/eibeyuf.jpg' /> everyone is beautiful. even you, ugly fish.</a></br>");
 });
 
 
